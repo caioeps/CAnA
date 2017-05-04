20c8e934b8a20bfd36b9fcc02d21206ac27242da
Caio Ergos <caioergos@gmail.com>
2017-05-04 01:57:35 -0300

    refactor more rubyish

--- karatsuba-ofman/karatsuba_ofman.rb
+++ karatsuba-ofman/karatsuba_ofman.rb
@@ -1,18 +1,14 @@
 module KaratsubaOfman
   def multiplication(u, v, n)
-    if n < 3
-      return u * v
-    else
-      m  = (n/2).ceil
-      p  = (u/(10**m)).floor
-      q  = u.modulo(10**m)
-      r  = (v/(10**m)).floor
-      s  = v.modulo(10**m)
-      pr = multiplication(p, r, m)
-      qs = multiplication(q, s, m)
-      y  = multiplication(p + q, r + s, m + 1)
-      x  = pr * (10**(2 * m)) + (y - pr - qs) * (10**m) + qs
-      return x
-    end
+    return u * v if n < 3
+    m  = (n/2).ceil
+    p  = (u/(10**m)).floor
+    q  = u.modulo(10**m)
+    r  = (v/(10**m)).floor
+    s  = v.modulo(10**m)
+    pr = multiplication(p, r, m)
+    qs = multiplication(q, s, m)
+    y  = multiplication(p + q, r + s, m + 1)
+    pr * (10**(2 * m)) + (y - pr - qs) * (10**m) + qs
   end
 end
